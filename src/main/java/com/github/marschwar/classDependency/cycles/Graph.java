bf5c29a9e4a14514b5f314932fe8d5a9922e17a8
Markus Schwarz <post@markus-schwarz.net>
2020-07-21 11:01:55 +0200

    implement cycle detection

---
+++ src/main/java/com/github/marschwar/classDependency/cycles/Graph.java
@@ -0,0 +1,86 @@
+package com.github.marschwar.classDependency.cycles;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptySet;
+
+public class Graph {
+	private final Map<String, Vertex> vertices;
+
+
+	private Graph(Map<String, Vertex> vertices) {
+		this.vertices = vertices;
+	}
+
+	public Collection<Vertex> getVertices() {
+		return Collections.unmodifiableCollection(vertices.values());
+	}
+
+	public Set<String> detectCycles() {
+		final HashSet<String> cycles = new HashSet<>();
+		final Collection<Vertex> verticesToVisit = this.vertices.values();
+		verticesToVisit.forEach(vertex -> vertex.setVisited(false));
+		verticesToVisit.forEach(vertex -> {
+			cycles.addAll(detectCycles(vertex, emptyList()));
+		});
+		return cycles;
+	}
+
+	private Collection<String> detectCycles(Vertex vertex, List<String> path) {
+		if (vertex.isVisited()) {
+			return emptySet();
+		}
+
+		final int index = path.indexOf(vertex.getId());
+		if (index > -1) {
+			return path.subList(index, path.size());
+		}
+
+		final Set<String> cycles = new HashSet<>();
+		vertex.getEdges().forEach(edge -> {
+			final ArrayList<String> newPath = new ArrayList<>(path);
+			newPath.add(vertex.getId());
+			cycles.addAll(detectCycles(edge, newPath));
+		});
+		vertex.setVisited(true);
+		return cycles;
+	}
+
+	@Override
+	public String toString() {
+		return vertices.values().toString();
+	}
+
+	public static class GraphBuilder {
+		private final Map<String, Vertex> vertices = new HashMap<>();
+
+		public GraphBuilder addEdge(String source, String target) {
+			final Vertex sourceVertex = getOrCreateVertex(source);
+			final Vertex targetVertex = getOrCreateVertex(target);
+			sourceVertex.addEdge(targetVertex);
+			return this;
+		}
+
+		private Vertex getOrCreateVertex(String id) {
+			Vertex vertex = vertices.get(id);
+			if (vertex == null) {
+				vertex = new Vertex(id);
+				vertices.put(id, vertex);
+			}
+			return vertex;
+		}
+
+
+		public Graph build() {
+			return new Graph(vertices);
+		}
+	}
+}
---
+++ src/test/java/com/github/marschwar/classDependency/cycles/GraphTest.java
@@ -0,0 +1,83 @@
+package com.github.marschwar.classDependency.cycles;
+
+import com.github.marschwar.classDependency.cycles.Graph.GraphBuilder;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GraphTest {
+
+	@Test
+	public void cycles_emptyGraph() {
+		final Graph graph = new GraphBuilder().build();
+
+		final Set<String> result = graph.detectCycles();
+
+		assertThat(result).isEmpty();
+	}
+
+	@Test
+	public void cycles_noCycle() {
+		final Graph graph = new GraphBuilder()
+				.addEdge("a", "b")
+				.addEdge("a", "c")
+				.addEdge("b", "c")
+				.addEdge("c", "d")
+				.addEdge("d", "e")
+				.addEdge("b", "e")
+				.build();
+
+		final Set<String> result = graph.detectCycles();
+
+		assertThat(result).isEmpty();
+	}
+
+	@Test
+	public void cycles_simpleCycle() {
+		final Graph graph = new GraphBuilder()
+				.addEdge("a", "b")
+				.addEdge("b", "a")
+				.build();
+
+		final Set<String> result = graph.detectCycles();
+
+		assertThat(result).containsOnly("a", "b");
+	}
+
+	@Test
+	public void cycles_cycleInTheEnd() {
+		final Graph graph = new GraphBuilder()
+				.addEdge("x", "a")
+				.addEdge("a", "b")
+				.addEdge("b", "c")
+				.addEdge("c", "a")
+				.build();
+
+		final Set<String> result = graph.detectCycles();
+
+		assertThat(result).containsOnly("a", "b", "c");
+	}
+
+	@Test
+	public void multipleCycles() {
+		final Graph graph = new GraphBuilder()
+				// first cycle
+				.addEdge("a", "b")
+				.addEdge("b", "c")
+				.addEdge("c", "a")
+
+				.addEdge("c", "d")
+				.addEdge("d", "e")
+
+				// second cycle
+				.addEdge("e", "f")
+				.addEdge("f", "e")
+				.build();
+
+		final Set<String> result = graph.detectCycles();
+
+		assertThat(result).containsOnly("a", "b", "c", "e", "f");
+	}
+}
